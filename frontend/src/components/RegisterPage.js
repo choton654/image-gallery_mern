654d308fd04b2295e21bb7fd4c5b5b91cd9216ab
root <root@DESKTOP-SaHeB.localdomain>
2021-01-30 13:38:20 +0530

    ..

--- frontend/src/components/RegisterPage.js
+++ frontend/src/components/RegisterPage.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { register } from '../redux/user/userAction';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { register } from "../redux/user/userAction";
 
 const RegisterPage = (props) => {
   const [user, setUser] = useState({
-    email: '',
-    name: '',
-    password: '',
+    email: "",
+    name: "",
+    password: "",
   });
 
   const dispatch = useDispatch();
@@ -14,9 +14,9 @@ const RegisterPage = (props) => {
 
   const { loading, isAuthenticated, error } = User;
 
-  useEffect(() => {
-    if (isAuthenticated) props.history.push('/images');
-  }, [isAuthenticated]);
+  // useEffect(() => {
+  //   if (isAuthenticated) props.history.push('/images');
+  // }, [isAuthenticated]);
 
   const handelChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
@@ -29,66 +29,66 @@ const RegisterPage = (props) => {
     // const { name, email, password } = user;
     dispatch(register(user));
 
-    // props.history.push('/images');
+    props.history.push("/dashboard");
     setUser({
-      email: '',
-      name: '',
-      password: '',
+      email: "",
+      name: "",
+      password: "",
     });
   };
 
   return (
-    <div className='container py-4'>
+    <div className="container py-4">
       {error ? (
-        <div className='text-center'>
-          <h2 className='text-light'>{error}</h2>
-          <h4 className='text-light'> refresh the page</h4>
+        <div className="text-center">
+          <h2 className="text-light">{error}</h2>
+          <h4 className="text-light"> refresh the page</h4>
         </div>
       ) : (
         <form onSubmit={handelSubmit}>
-          <div className='form-group'>
-            <label className='text-light' htmlFor='exampleInputEmail1'>
+          <div className="form-group">
+            <label className="text-light" htmlFor="exampleInputEmail1">
               Email address
             </label>
             <input
-              type='email'
+              type="email"
               onChange={handelChange}
-              name='email'
+              name="email"
               value={user.email}
-              className='form-control'
-              placeholder='Enter email'
+              className="form-control"
+              placeholder="Enter email"
               required
             />
           </div>
-          <div className='form-group'>
-            <label className='text-light' htmlFor='exampleInputName'>
+          <div className="form-group">
+            <label className="text-light" htmlFor="exampleInputName">
               Name
             </label>
             <input
-              type='text'
+              type="text"
               onChange={handelChange}
-              name='name'
+              name="name"
               value={user.name}
-              className='form-control'
-              placeholder='Enter name'
+              className="form-control"
+              placeholder="Enter name"
               required
             />
           </div>
-          <div className='form-group'>
-            <label className='text-light' htmlFor='exampleInputPassword1'>
+          <div className="form-group">
+            <label className="text-light" htmlFor="exampleInputPassword1">
               Password
             </label>
             <input
-              type='password'
+              type="password"
               onChange={handelChange}
-              name='password'
+              name="password"
               value={user.password}
-              className='form-control'
-              placeholder='Password'
+              className="form-control"
+              placeholder="Password"
               required
             />
           </div>
-          <button type='submit' className='btn btn-primary'>
+          <button type="submit" className="btn btn-primary">
             Submit
           </button>
         </form>
